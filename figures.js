d7e9fb838a3c63f25d8da38c8b90a05028a29d5e
alexsencion <alexsencion99@gmail.com>
2022-03-02 12:59:07 -0400

    Encapsulating code in functions

--- figures.js
+++ figures.js
@@ -4,14 +4,20 @@
 
 console.group("Cuadrados");
 
-const squareSide = 5;
-console.log("The square side measure: " + squareSide + "cm");
+// const squareSide = 5;
+// console.log("The square side measure: " + squareSide + "cm");
 
-const squarePerimeter = squareSide * 4;
-console.log("The perimeter es: " + squarePerimeter + "cm");
+function squarePerimeter(side) {
+    return side * 4;
+} 
 
-const squareArea = squareSide * squareSide;
-console.log("The squareArea es: " + squareArea + "cm^2");
+// console.log(squarePerimeter(56))
+// console.log("The perimeter es: " + squarePerimeter + "cm");
+
+function squareArea(side) {
+    return side * side;
+}
+// console.log("The squareArea es: " + squareArea + "cm^2");
 
 
 console.groupEnd();
@@ -21,27 +27,39 @@ console.groupEnd();
 console.group("Triangulo");
 
 
-const triangleSide1 = 6;
-const triangleSide2 = 6;
-const trianglebase = 4;
+// const triangleSide1 = 6;
+// const triangleSide2 = 6;
+// const trianglebase = 4;
+
+console.log(
+    trianglePerimeter(6,6,4))
+
+    
+console.log(
+    triangleArea(4,5.5))
 
 
+// console.log("The triangle side measure: " 
+// + triangleSide1 + "cm, " 
+// + triangleSide2 + "cm, " 
+// + trianglebase + "cm"
 
-console.log("The triangle side measure: " 
-+ triangleSide1 + "cm, " 
-+ triangleSide2 + "cm, " 
-+ trianglebase + "cm"
+// );
 
-);
+// const triangleHeight = 5.5;
+// console.log("The triangle height is: " + triangleHeight + "cm");
 
-const triangleHeight = 5.5;
-console.log("The triangle height is: " + triangleHeight + "cm");
+function trianglePerimeter(side1,  side2, side3) {
+    return side1 + side2 + side3
+}
 
-const trianglePerimeter = triangleSide1 + triangleSide2 + trianglebase;
-console.log("The perimeter of triangle is: " + trianglePerimeter + "cm");
+// console.log("The perimeter of triangle is: " + trianglePerimeter + "cm");
 
-const triangleArea = (trianglebase * triangleHeight) / 2;
-console.log("The area of triangle is: " + triangleArea + "cm^2");
+// const triangleArea = (trianglebase * triangleHeight) / 2;
+function triangleArea(base,  height) {
+    return (base * height) / 2
+}
+// console.log("The area of triangle is: " + triangleArea + "cm^2");
 
 
 console.groupEnd();
@@ -50,15 +68,21 @@ console.groupEnd();
 // Codigo del Circulo
 console.group("Circle");
 
-// Radie
-const circleRadio = 4;
-console.log("The circleRadio is: " + circleRadio + "cm");
+// Radio
+// const circleRadio = 4;
+// console.log("The circleRadio is: " + circleRadio + "cm");
+
+
+
 
 
 // Diametro
 
-const diameter = circleRadio * 2;
-console.log("The diameter is: " + diameter + "cm");
+// const diameter = circleRadio * 2;
+function diameterCircle(radio) {
+    return radio * 2
+}
+// console.log("The diameter is: " + diameter + "cm");
 
 
 // PI
@@ -67,17 +91,31 @@ const PI = Math.PI;
 console.log("The PI is: " + PI);
 
 //Circunferencia
-const circlePerimeter = diameter * PI;
-console.log("The circlePerimeter is: " + circlePerimeter + "cm");
+// const circlePerimeter = diameter * PI;
+function circlePerimeter(radio) {
+    const diameter = diameterCircle(radio) 
+        return diameter * PI
+    
+}
+
+// console.log("The circlePerimeter is: " + circlePerimeter + "cm");
 
 
 
 // Area
 
-const circleArea = (circleRadio * circleRadio) * PI;
-console.log("The circleArea is: " + circleArea + "cm^2");
+function circleArea(radio) {
+    return (radio * radio) * PI
+}
 
 
+// const circleArea = (circleRadio * circleRadio) * PI;
+// console.log("The circleArea is: " + circleArea + "cm^2");
 
+
+console.log(
+    circleArea(5))
 
 console.groupEnd();
+
+
